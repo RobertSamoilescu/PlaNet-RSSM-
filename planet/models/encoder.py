871d72a32ae3a8780ccf460e850ce2bf4a63f982
Robert Samoilescu <robert.samoilescu@gmail.com>
2024-08-07 15:56:49 +0100

    Included image encoder model.

--- planet/models/encoder.py
+++ planet/models/encoder.py
@@ -24,3 +24,57 @@ class EncoderModel(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         return self.mean_head(x), self.log_std_head(x)
+
+
+class Encoder(nn.Module):
+    def __init__(self, in_channels: int = 3, d_latent: int = 128):
+        super(Encoder, self).__init__()
+        self.d_latent = d_latent
+        self.in_channels = in_channels
+
+        modules = []
+        hidden_dims = [32, 64, 128, 256, 512]
+
+        # Build Encoder
+        for h_dim in hidden_dims:
+            modules.append(
+                nn.Sequential(
+                    nn.Conv2d(in_channels, out_channels=h_dim, kernel_size=3, stride=2, padding=1),
+                    nn.BatchNorm2d(h_dim),
+                    nn.LeakyReLU())
+            )
+            in_channels = h_dim
+
+        self.encoder = nn.Sequential(*modules)
+        self.fc = nn.Linear(hidden_dims[-1] * 4, self.d_latent)
+
+    def forward(self, x) -> Tensor:
+        x = self.encoder(x)
+        x = x.view(x.shape[0], -1)
+        return self.fc(x)
+
+
+class ImageEncoderModel(nn.Module):
+    def __init__(
+        self,
+        hidden_state_size: int,
+        observation_size: int,
+        state_size: int,
+        hidden_layer_size: int,
+        in_channels: int = 3,
+    ) -> None:
+        super(ImageEncoderModel, self).__init__()
+        input_size = hidden_state_size + observation_size
+        
+        self.obs_encoder = Encoder(in_channels=in_channels, d_latent=observation_size)
+        self.fc1 = nn.Linear(input_size, hidden_layer_size)
+        self.fc2 = nn.Linear(hidden_layer_size, hidden_layer_size)
+        self.mean_head = nn.Linear(hidden_layer_size, state_size)
+        self.log_std_head = nn.Linear(hidden_layer_size, state_size)
+
+    def forward(self, hidden_state: Tensor, observation: Tensor) -> Tensor:
+        observation = self.obs_encoder(observation)
+        x = torch.cat([hidden_state, observation], dim=-1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.mean_head(x), self.log_std_head(x)
