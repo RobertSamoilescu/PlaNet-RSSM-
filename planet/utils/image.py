fd0214174bde72728829a6ca20319d716f6a4566
Robert Samoilescu <robert.samoilescu@gmail.com>
2024-08-07 17:16:56 +0100

    Included 5 bit reducer

---
+++ planet/utils/image.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def preprocess_obs(obs, bit_depth: int = 5):
+    """
+    Reduces the bit depth of image for the ease of training
+    and convert to [-0.5, 0.5]
+    In addition, add uniform random noise same as original implementation
+    """
+    obs = obs.astype(np.float32)
+    reduced_obs = np.floor(obs / 2 ** (8 - bit_depth))
+    normalized_obs = reduced_obs / 2**bit_depth - 0.5
+    normalized_obs += np.random.uniform(0.0, 1.0 / 2**bit_depth, normalized_obs.shape)
+    return normalized_obs
